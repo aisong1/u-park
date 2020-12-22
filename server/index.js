d28d2d015d561979c3a6ec059124851abb390c0a
Andrew Song <andrewsong61@gmail.com>
2020-12-21 17:07:40 -0800

    Configure api endpoint to query database

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const parser = require('body-parser');
+const parking = require('../database/index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,17 @@ app.use(morgan('dev'));
 app.use(parser.json());
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+app.get('/api/parking', (req, res) => {
+  const query = "TODO";
+  const params = [];
+  parking.query(query, (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+    res.send(data);
+  });
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
