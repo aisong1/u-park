494cc3abb08b8b997a2a4e6b8fdf277f9626d250
Andrew Song <andrewsong61@gmail.com>
2020-12-31 16:59:11 -0800

    Add Markers for each meter that is most likely to be available

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 import styles from './styles/Map.css';
 
 mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
@@ -7,7 +8,7 @@ mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
 const App = (props) => {
   const [lng, setLng] = useState(-122.3321);
   const [lat, setLat] = useState(47.6062);
-  const [zoom, setZoom] = useState(10);
+  const [zoom, setZoom] = useState(11);
 
   const mapContainer = useRef(null);
 
@@ -23,8 +24,33 @@ const App = (props) => {
     // Add navigation control (+/- buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
+    // Fetch data from API
+    axios.get('/api/parking')
+      .then(({ data }) => {
+        const { rows } = data;
+        rows.forEach((row) => {
+          new mapboxgl.Marker({
+            color: "#FFFFFF",
+          })
+          .setPopup(
+            new mapboxgl.Popup({ 
+              className: 'popup' 
+            }).setHTML(
+              `<p><b>Meter Code</b>: ${row.meter_code}</p>
+              <p><b>Street</b>: ${row.blockface_name}</p>
+              <p><b>Side of Street</b>: ${row.side_of_street}</p>`
+            )
+          )
+          .setLngLat([row.longitude, row.latitude])
+          .addTo(map);
+        })
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+
     // Clean up map on unmount
-    return () => map.remove();
+    // return () => map.remove();
   }, []);
 
   return (
