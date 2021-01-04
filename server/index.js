579dc5a1e065b289a5626bcc6f072a775a0017d9
Andrew Song <andrewsong61@gmail.com>
2021-01-04 11:21:39 -0800

    Refactor to use pg-promise; remove unnecessary middleware

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
 const parser = require('body-parser');
 const parking = require('../database/index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(morgan('dev'));
 app.use(parser.json());
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
@@ -14,28 +12,30 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.get('/api/parking', (req, res) => {
   const query = "SELECT * FROM least_likely";
 
-  parking.query(query, (err, data) => {
-    if (err) {
-      console.log('Failed to get parking status: ', err);
+  parking.any(query)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log('Failed to retrieve parking status: ', err);
       res.end();
-    }
-    res.send(data);
-  });
+    });
 });
 
 app.get('/api/parking/users', (req, res) => {
-  const { email, password } = req.body;
-  const query = `SELECT id FROM users WHERE email='$1' AND password=crypt('$2', password);`;
-  const params = [email, password]
+  const { email, password } = req.query;
+  const query = "SELECT firstName FROM users WHERE email=$1 AND password=crypt($2, password);";
+  const params = [email, password];
 
-  parking.query(query, params, (err, data) => {
-    if (err) {
-      throw new Error(err);
+  parking.any(query, params)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log('Failed to retrieve user information: ', err);
       res.end();
-    }
-    res.send(data);
-  })
-})
+    });
+});
 
 app.post('/api/parking/users', (req, res) => {
   const { firstName, lastName, email, password } = req.body;
@@ -47,13 +47,11 @@ app.post('/api/parking/users', (req, res) => {
   );`;
   const params = [firstName, lastName, email, password];
 
-  parking.query(query, params, (err, data) => {
-    if (err) {
-      console.log('Failed to create user: ', err);
+  parking.any(query, params)
+    .catch((err) => {
+      console.log('Failed to create new user: ', err);
       res.end();
-    }
-    res.send('User successfully created.');
-  });
+    });
 });
 
 app.listen(PORT, () => {
