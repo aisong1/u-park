9cd2b7ba0426ca409c904f966b4d5d9a3ee5d08f
Andrew Song <andrewsong61@gmail.com>
2021-01-04 11:19:56 -0800

    Add state to handle submission

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -2,25 +2,38 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const LoginForm = ({ handleLoginSubmit, handleBackClick }) => (
-  <div>
-    <Form onSubmit={(e) => handleLoginSubmit(e)}>
-      <Form.Group controlId="loginEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group controlId="loginPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="dark" type="submit">
-        Submit
-      </Button>
-      <Button variant="outline-secondary" type="submit" onClick={handleBackClick}>
-        Back
-      </Button>
-    </Form>
-  </div>
-)
+const LoginForm = ({ handleLoginSubmit, handleBackClick }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <Form onSubmit={(e) => handleLoginSubmit(e, email, password)}>
+        <Form.Group controlId="loginEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control 
+            type="email" 
+            placeholder="Enter email" 
+            onChange={setEmail}
+          />
+        </Form.Group>
+        <Form.Group controlId="loginPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            type="password" 
+            placeholder="Password" 
+            onChange={setPassword}
+          />
+        </Form.Group>
+        <Button variant="dark" type="submit">
+          Submit
+        </Button>
+        <Button variant="outline-secondary" type="submit" onClick={handleBackClick}>
+          Back
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginForm;
