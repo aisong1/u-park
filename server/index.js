bfc11b87e97c2f79645a96ef99863d6af5f5c506
Andrew Song <andrewsong61@gmail.com>
2021-01-03 19:06:36 -0800

    Add API endpoints for user authentication

--- server/index.js
+++ server/index.js
@@ -13,13 +13,48 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 app.get('/api/parking', (req, res) => {
   const query = "SELECT * FROM least_likely";
+
   parking.query(query, (err, data) => {
     if (err) {
-      throw new Error(err);
+      console.log('Failed to get parking status: ', err);
+      res.end();
     }
     res.send(data);
   });
+});
+
+app.get('/api/parking/users', (req, res) => {
+  const { email, password } = req.body;
+  const query = `SELECT id FROM users WHERE email='$1' AND password=crypt('$2', password);`;
+  const params = [email, password]
+
+  parking.query(query, params, (err, data) => {
+    if (err) {
+      throw new Error(err);
+      res.end();
+    }
+    res.send(data);
+  })
 })
+
+app.post('/api/parking/users', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const query = `INSERT INTO users (firstName, lastName, email, password) VALUES (
+    $1,
+    $2,
+    $3,
+    crypt($4, gen_salt('md5'))
+  );`;
+  const params = [firstName, lastName, email, password];
+
+  parking.query(query, params, (err, data) => {
+    if (err) {
+      console.log('Failed to create user: ', err);
+      res.end();
+    }
+    res.send('User successfully created.');
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
