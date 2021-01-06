e81fc1f20ceb9bc7a822b3f581996263c7f6fba4
Andrew Song <andrewsong61@gmail.com>
2021-01-06 11:09:58 -0800

    Create Favorites component

--- client/components/Favorites.jsx
+++ client/components/Favorites.jsx
@@ -4,14 +4,27 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import axios from 'axios';
 import styles from './styles/Map.css';
 
-const Favorites = () => {
+const Favorites = ({ favorites }) => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.heading}>
         <h1>Favorites</h1>
       </div>
       <div className={styles.favorites}>
-        
+        {favorites.map((favorite) => {
+          const props = favorite.properties;
+          const street = props.blockface_name;
+          const pattern = 'BETWEEN';
+          const title = street.slice(0, street.indexOf(pattern));
+          return (
+            <div key={`favorite-${props.id}`} className={styles.item}>
+              <a href="#" className={styles.title} id={`link-${props.id}`}>{title} · {props.side_of_street}</a>
+              <div className={styles.desc}>
+                {props.meter_code}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
